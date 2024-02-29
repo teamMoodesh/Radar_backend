50fc24b11a8bea422482109631e662fa6342667f
unknown <abhijithrajesh18@gmail.com>
2024-03-01 01:46:17 +0530

    login logout auth

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
@@ -16,12 +16,13 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-const Routes = require('./routes/personalRoutes');
-
+const Routes = require("./routes/personalRoutes");
+const userRouter = require("./routes/userRoutes");
 
 app.get("/", (req, res) => {
   res.send("hello World");
 });
 
-app.use("/api/v1", Routes)
+app.use("/api/v1", Routes);
+app.use("/api/v1/user", userRouter);
 module.exports = { app };
